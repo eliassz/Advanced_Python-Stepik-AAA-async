0c4813593717dcfc1bba4c80aa0216b1f1980b23
Ilya Lomonosov <lomonosov.iv@phystech.edu>
2024-01-04 18:46:22 +0400

    [TASK_4]

--- homework/tasks/task_4.py
+++ homework/tasks/task_4.py
@@ -1,32 +1,30 @@
-async def task_1(i: int):
+async def task_1(i: int, order: list):
+    order.append("1")
     if i == 0:
         return
-
     if i > 5:
-        await task_2(i // 2)
+        await task_2(i // 2, order)
     else:
-        await task_2(i - 1)
+        await task_2(i - 1, order)
 
 
-async def task_2(i: int):
+async def task_2(i: int, order: list):
+    order.append("2")
     if i == 0:
         return
-
     if i % 2 == 0:
-        await task_1(i // 2)
+        await task_1(i // 2, order)
     else:
-        await task_2(i - 1)
+        await task_2(i - 1, order)
 
 
 async def coroutines_execution_order(i: int = 42) -> int:
-    # Отследите порядок исполнения корутин при i = 42 и верните число, соответствующее ему.
-    #
-    # Когда поток управления входит в task_1 добавьте к результату цифру 1, а когда он входит в task_2,
-    # добавьте цифру 2.
-    #
-    # Пример:
-    # i = 7
-    # return 12212
-    await task_1(i)
+    order = []
+    await task_1(i, order)
+    return int("".join(order))
 
-    # YOUR CODE GOES HERE
+if __name__ == "__main__":
+    import asyncio
+
+    res = asyncio.run(coroutines_execution_order(7))
+    print(res)
